e5c48e7c50fc1de348e06cd50f6e1cc3f9f793df
Augusto Santos <augusto_santos55@hotmail.com>
2017-04-20 08:30:01 -0300

    update

---
+++ react-ex/ex/components/component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Teste extends React.Component{
+    render(){
+        return(
+            <h1>aqui tem um component!</h1>
+        )
+    }
+}
+
+export default Teste
--- react-ex/ex/index.jsx
+++ react-ex/ex/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Comp from './components/component.jsx'
 
 ReactDOM.render(
     <div className={`box`}>
         <h1 className={`title`}>Olá React</h1>
+        <Comp />
     </div>, 
 
 document.getElementById('root'))
